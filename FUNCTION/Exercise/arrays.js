4be3463bf400d577ccc1336ef19207b1d701380b
Jean <damour2014stage@gmail.com>
2022-09-08 13:51:20 -0500

    ...

--- FUNCTION/Exercise/arrays.js
+++ FUNCTION/Exercise/arrays.js
@@ -120,17 +120,31 @@ let expr = "([{}]))";
 console.log(areBracketsBalanced(expr));
 
 //#################################################################
-function getMaxSubSum(arr) {
-let listSums = [];
-for(let i = 0 ; i < arr.length; i++){
-let sum = 0;
-for(let j = i ; j < arr.length ; j++){
-sum += +arr[j]
-listSums.push(sum);
-}
-}
-return(Math.max(...listSums));
-}
+/**
+ * The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
 
-const data = [-1,-2,-3];
-console.log(getMaxSubSum(data));
+The task is: find the contiguous subarray of arr with the maximal sum of items.
+
+Write the function getMaxSubSum(arr) that will return that sum.
+ * @param {*} arr 
+ * @returns 
+ */
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+  
+    for (let item of arr) { // for each item of arr
+      partialSum += item; // add it to partialSum
+      maxSum = Math.max(maxSum, partialSum); // remember the maximum
+      if (partialSum < 0) partialSum = 0; // zero if negative
+    }
+  
+    return maxSum;
+  }
+  
+  console.log( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+  console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+  console.log( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+  console.log( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+  console.log( getMaxSubSum([1, 2, 3]) ); // 6
+  console.log( getMaxSubSum([-1, -2, -3]) ); // 0
