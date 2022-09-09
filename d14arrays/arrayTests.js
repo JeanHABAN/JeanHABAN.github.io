c04a05cf62eadd28c5371975fc52aa4ddab04419
Jean <damour2014stage@gmail.com>
2022-09-08 20:03:04 -0500

    Update arrayTests.js

--- d14arrays/arrayTests.js
+++ d14arrays/arrayTests.js
@@ -136,17 +136,17 @@ describe("score exam", function () {
 });
 
 // /* 6. Write a function that takes two integers as inputs and returns a 2-dimensional array containing sequential numbers across each row as follows: */
-// describe("generate array", function () {
-//     const expected33 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];
-//     const expected23 = [ [1, 2, 3], [4, 5, 6]];
-//     const expected21 = [ [1], [2]];
-//     it("expected33", function () {
-//         assert.deepEqual(generateArray(3,3), expected33);
-//     });
-//     it("expected23", function () {
-//         assert.deepEqual(generateArray(2,3), expected23);
-//     });
-//     it("expected21", function () {
-//         assert.deepEqual(generateArray(2, 1), expected21);
-//     });
-// });
+describe("generate array", function () {
+    const expected33 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const expected23 = [ [1, 2, 3], [4, 5, 6]];
+    const expected21 = [ [1], [2]];
+    it("expected33", function () {
+        assert.deepEqual(generateArray(3,3), expected33);
+    });
+    it("expected23", function () {
+        assert.deepEqual(generateArray(2,3), expected23);
+    });
+    it("expected21", function () {
+        assert.deepEqual(generateArray(2, 1), expected21);
+    });
+});
