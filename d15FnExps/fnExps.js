c52e1196ab77715ef984a9465a90e76dd24f833e
Jean <damour2014stage@gmail.com>
2022-09-11 20:41:59 -0500

    qi

--- d15FnExps/fnExps.js
+++ d15FnExps/fnExps.js
@@ -1,8 +1,9 @@
+/* eslint-disable*/;
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
 */ 
-module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
+//module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
 
 /**
  *@returns {number} double the input
